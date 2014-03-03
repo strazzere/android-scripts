37f189e2a7975ca85fc47de38cc5c2d359b3c323
Tim Strazzere <Tim.Strazzere@mylookout.com>
2014-03-03 13:10:04 -0800

    Adding reference to other sample and other encrypted strings

--- Decoders/MuchSad/dogekrypt.java
+++ Decoders/MuchSad/dogekrypt.java
@@ -1,11 +1,12 @@
 /**
- * "Krypt" class from the "MuchSad.A" Android malware'
+ * "Krypt" class from the CoinKrypt.A/B (MuchSad.A) Android malware
  * 		used to decrypt the simply obfuscated strings
  * 		in the Main class. Most used for the tracker/
  * 		mining args
  *
  *              Strings / Code reversed from
  *              SHA1: bc27f10178e2bfae277a5b97f1730da5822e4543
+ *                    cc8c9ea51a2bdefb4ce9b866eec73a02571bc344
  * 
  * @author Tim "diff" Strazzere
  * 	   <diff@lookout.com>
@@ -14,17 +15,27 @@
 public class dogekrypt {
 
 	public static void main(String[] args) {
-		String[] encrypted = {
-				"uggc://cbbyfnghea.serr-u.arg/cbby.cuc?c=qngn",
-				"uggc://cbbyfnghea.serr-u.arg/hcqngr.cuc?c=qngn",
-				"/yvopchzvare.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d",
-				"/yvopchzvare.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d;",
-				"/yvopchzvareARBA.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d",
-				"/yvopchzvareARBA.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d;",
-				"uggc://cbbyfnghea.serr-u.arg/ertvfgre.cuc?vq="
-		};
-		for( String crypted : encrypted)
-			System.out.println(doConvert(crypted));
+	    String[] encrypted_sample1 = {
+		"uggc://cbbyfnghea.serr-u.arg/cbby.cuc?c=qngn",
+		"uggc://cbbyfnghea.serr-u.arg/hcqngr.cuc?c=qngn",
+		"/yvopchzvare.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d",
+		"/yvopchzvare.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d;",
+		"/yvopchzvareARBA.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d",
+		"/yvopchzvareARBA.fb --nytb=fpelcg -b uggc://sybj.ybtvagb.zr:9555 -B Q7A6H92Ncdju1NzO4ElMKfIAXUfYDuTYeN:k -e 20 -g 1 -O -d;",
+		"uggc://cbbyfnghea.serr-u.arg/ertvfgre.cuc?vq="
+	    };
+
+	    String[] encrypted_sample2 = {
+		"/FreivprCebivqre --hey=uggc://91.234.105.69:9327 --hfrecnff=YoULkLThLHRReqcx9L63cvW1N4dW3gAtw1:k --guernqf=1 --ergevrf=5;",
+		"/FreivprCebivqre --hey=uggc://91.234.105.69:9327 --hfrecnff=YoULkLThLHRReqcx9L63cvW1N4dW3gAtw1:k --guernqf=1 --ergevrf=5",
+		"fbheaq.ab-vc.ovm"
+	    };
+
+	    for( String crypted : encrypted_sample1)
+		System.out.println(doConvert(crypted));
+
+	    for( String crypted : encrypted_sample2)
+		System.out.println(doConvert(crypted));
 	}
 	
 	private static byte abyte;
@@ -55,5 +66,4 @@ public class dogekrypt {
 
 		return tempReturn.toString();
 	}
-
 }
