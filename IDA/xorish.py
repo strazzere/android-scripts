1e60fa42621545ba2df151c0c314c512f5a3883f
Tim Strazzere <tim@strazzere.com>
2015-11-10 14:33:04 -0800

    Extend key for larger than key strings.

--- IDA/xorish.py
+++ IDA/xorish.py
@@ -7,10 +7,7 @@
 #
 
 def strxor(a, b):
-    if len(a) > len(b):
-        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
-    else:
-        return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
+    return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b*100)])
 
 def xor():
     print ' ..'
